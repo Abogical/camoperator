a726538343eff536592afd0c0253bd1c6eda2fb0
Abdelrahman Abdelrahman <git@abogic.al>
2024-02-18 10:46:20 -0330

    Download image from camera while its moving

--- camoperator/main.py
+++ camoperator/main.py
@@ -5,6 +5,7 @@ import os
 from itertools import cycle
 import numpy as np
 from tqdm import tqdm
+import threading
 
 argument_parser = argparse.ArgumentParser(
     prog='camoperator',
@@ -43,25 +44,43 @@ def get_steps(min, max, divisions):
     positions = np.round(np.linspace(min, max, divisions))
     return (positions[1:]-positions[:-1]).astype(int)
 
-def capture_xy(camera, directory, x, y):
+class DownloadThread(threading.Thread):
+    def __init__(self, camera, source, destination, progress):
+        threading.Thread.__init__(self)
+        self.camera = camera
+        self.source = source
+        self.destination = destination
+        self.progress = progress
+    
+    def run(self):
+        self.camera.download(self.source, self.destination)
+        self.progress.update(1)
+
+download_thread = None
+def capture_xy(camera, directory, x, y, progress):
+    global download_thread
+    if download_thread != None:
+        download_thread.join()
+
     capture_file = camera.capture()
-    camera.download(
+    download_thread = DownloadThread(
+        camera,
         capture_file,
-        os.path.join(directory, f"{x}-{y}.png")
-    ) # Could be done in another thread
+        os.path.join(directory, f"{x}-{y}.png"),
+        progress
+    )
+    download_thread.start()
 
 def capture_x_axis(camera, directory, y, x_steps, max_x, controller, progress):
     even_y = (y%2 == 0)
     
     progress.set_description(f'Capturing images. Current (0, {y})')
-    capture_xy(camera, directory, 0 if even_y else max_x, y)
-    progress.update(1)
+    capture_xy(camera, directory, 0 if even_y else max_x, y, progress)
 
     for x, x_step in (enumerate(x_steps, start=1) if even_y else zip(range(max_x-1, -1, -1), reversed(-x_steps))):
         controller.move_x(x_step)
         progress.set_description(f'Capturing images. Current ({x}, {y})')
-        capture_xy(camera, directory, x, y)
-        progress.update(1)
+        capture_xy(camera, directory, x, y, progress)
 
 def main():
     arguments = argument_parser.parse_args()
@@ -80,6 +99,7 @@ def main():
         controller.move_y(y_step)
         capture_x_axis(camera, arguments.directory, y, x_steps, arguments.horizontal_images-1, controller, progress)
     
+    download_thread.join()
     camera.close()
     controller.close()
         
